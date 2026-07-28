b13038de810fc2cd51170ba0383b42b2116f85f5
Ties den Ouden <denouden.ties@gmail.com>
2026-07-29 00:26:47 +0200

    Basic module type registration

--- register_types.h
+++ register_types.h
@@ -2,3 +2,8 @@
 #define AST_DEBUGGER_REGISTER_TYPES_H
 
 #include "modules/register_module_types.h"
+
+void initialize_ast_debugger_module(ModuleInitializationLevel p_level);
+void uninitialize_ast_debugger_module(ModuleInitializationLevel p_level);
+
+#endif // AST_DEBUGGER_REGISTER_TYPES_H
