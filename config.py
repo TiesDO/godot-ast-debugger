d0e6fb76df543611fc8308e8a6353f4d88dd226d
Ties den Ouden <denouden.ties@gmail.com>
2026-07-29 00:15:57 +0200

    Init

---
+++ config.py
@@ -0,0 +1,5 @@
+def can_build() -> bool:
+    return True
+
+def configure(env):
+    pass
---
+++ register_types.h
@@ -0,0 +1,4 @@
+#ifndef AST_DEBUGGER_REGISTER_TYPES_H
+#define AST_DEBUGGER_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
